874d435b6b2764e9ce1410c3f5712758fc55a651
Nate Lovell <Natelovell99@gmail.com>
2021-03-18 03:41:25 -0600

    Create AME Front End and Batch Item Objects.jsx

---
+++ AME Front End and Batch Item Objects.jsx
@@ -0,0 +1,42 @@
+﻿// Front End
+var frontEnd = app.getFrontend();
+// this will allow us to add items to the render queue
+// Methods
+frontEnd.addDLToBatch(project, format, preset, guid, destination);
+frontEnd.addCompToBatch(project, preset, destination);
+frontEnd.addFileToBatch(file, format, preset, destination);
+frontEnd.addFileSequenceToBatch();
+frontEnd.addXMLToBatch();
+frontEnd.addTeamProjectsItemToBatch();
+frontEnd.stitchFiles();
+frontEnd.stopBatch();
+frontEnd.getDLItemsAtRoot(projectPath);
+
+// Batch Item
+// this is the data returned after you add something to the render queue
+// Attributes
+batchItem.outputFiles;
+batchItem.encodeProgress;
+batchItem.outputWidth;
+batchItem.outputHeight;
+// Methods
+batchItem.getEncodeProgress();
+batchItem.getEncodeTime();
+batchItem.getCurrentBatchPreview();
+batchItem.getPresetList();
+batchitem.getMissingAssets();
+batchItem.loadPreset();
+batchItem.loadFormat();
+batchItem.setWorkArea();
+batchItem.setUsePreviewFiles();
+batchItem.setUseMaximumRenderQuality();
+batchItem.setUseFrameBlending();
+batchItem.setIncludeSourceXMP();
+batchItem.setIncludeSourceCuePoints();
+batchItem.setCropState();
+batchItem.setCropType();
+batchItem.setCropOffsets();
+batchItem.setOutputFrameSize();
+batchItem.setXMPData();
+batchItem.setCuePointData();
+batchItem.setScaleType();
